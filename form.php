6bcd6039edcfdc164c04b4f261348d713d38e6a1
Hiroto Taguchi <zigzag37458@gmail.com>
2020-12-14 19:34:14 +0900

    [add] 初めてのコミット
    ステージングエリアからローカルリポジトリへの記録

---
+++ form.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>Form</title>
+</head>
+<body>
+<form action="display.php" method="get">
+<label for="your_name">お名前：<label>
+<input type="text" id="your_name" name="your_name" maxlength="197" value="">
+<input type="submit" value="送信">
+</form>
+</body>
+</html>
