571e5c69f20fddb36b638e94ae03207b42fe733e
Hiroto Taguchi <zigzag37458@gmail.com>
2020-12-14 19:50:58 +0900

    [update] 記入指示の追加
    ユーザーにわかりやすいように「お名前をご記入ください」と追加しました

--- form.php
+++ form.php
@@ -5,6 +5,7 @@
 <title>Form</title>
 </head>
 <body>
+<p>あなたのお名前をご記入ください。</p>
 <form action="display.php" method="get">
 <label for="your_name">お名前：<label>
 <input type="text" id="your_name" name="your_name" maxlength="197" value="">
