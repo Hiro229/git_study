9ae35e53f09721f93973a84bfd8b0139502dc996
Hiroto Taguchi <zigzag37458@gmail.com>
2020-12-14 19:44:16 +0900

    [update] フォームの削除
    入力フォームを削除しform.phpへのリンクを加えました
    DOCTYPE宣言を小文字から大文字に変更しました

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="ja">
 <head>
 <meta charset="utf-8">
-<title>Form</title>
+<title>index</title>
 </head>
 <body>
-<form action="display.php" method="get">
-<label for="your_name">お名前：<label>
-<input type="text" id="your_name" name="your_name" maxlength="197" value="">
-<input type="submit" value="送信">
-</form>
+<a href="form.php">入力はこちら</a>
 </body>
 </html>
